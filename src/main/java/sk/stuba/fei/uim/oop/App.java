a30c37b5025e5ee383ebb6d2d88946bcfe99e70a
Matus-Zlacky <xzlacky@stuba.sk>
2021-03-01 14:16:09 +0100

    Podiel

--- src/main/java/sk/stuba/fei/uim/oop/App.java
+++ src/main/java/sk/stuba/fei/uim/oop/App.java
@@ -4,7 +4,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hellow World");
 
-
         int celeCislo = 9;
         long celeCisloVacsie = 4L;
         float desatinneSinglePrecision = 5.5f;
@@ -13,6 +12,13 @@ public class App {
         String retazec = "a";
 
         int podiel = 2/3; // je 0
+        System.out.println(podiel);
         int poDeleni = 2%3; // je 2
+        System.out.println(poDeleni);
+
+        float podielDesatiny = 2/3.0f;
+        System.out.println(podielDesatiny);
+
+
     }
 }
