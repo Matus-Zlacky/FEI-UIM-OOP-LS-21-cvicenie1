348d6fd70ad049c53ec8f1674904ec65a3eda962
Matus-Zlacky <xzlacky@stuba.sk>
2021-03-01 14:19:11 +0100

    if else

--- src/main/java/sk/stuba/fei/uim/oop/App.java
+++ src/main/java/sk/stuba/fei/uim/oop/App.java
@@ -2,22 +2,11 @@ package sk.stuba.fei.uim.oop;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hellow World");
+        if(2 == 1)
+            System.out.println("pravda");
+        else
+            System.out.println("nepravda");
 
-        int celeCislo = 9;
-        long celeCisloVacsie = 4L;
-        float desatinneSinglePrecision = 5.5f;
-        double desatinneDoublePrecision = 5.5;
-        char znak = 'A';
-        String retazec = "a";
-
-        int podiel = 2/3; // je 0
-        System.out.println(podiel);
-        int poDeleni = 2%3; // je 2
-        System.out.println(poDeleni);
-
-        float podielDesatiny = 2/3.0f;
-        System.out.println(podielDesatiny);
 
 
     }
