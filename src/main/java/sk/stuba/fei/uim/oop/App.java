4ce161e45cde142ac5c9b87c4d085c42e1daaa9a
Matus-Zlacky <xzlacky@stuba.sk>
2021-03-01 14:14:02 +0100

    Typy premennych

--- src/main/java/sk/stuba/fei/uim/oop/App.java
+++ src/main/java/sk/stuba/fei/uim/oop/App.java
@@ -4,5 +4,15 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hellow World");
 
+
+        int celeCislo = 9;
+        long celeCisloVacsie = 4L;
+        float desatinneSinglePrecision = 5.5f;
+        double desatinneDoublePrecision = 5.5;
+        char znak = 'A';
+        String retazec = "a";
+
+        int podiel = 2/3; // je 0
+        int poDeleni = 2%3; // je 2
     }
 }
